2af0dc18f3cc3177cf969715605fe64979df9a21
GoGiants1 <chooi221@snu.ac.kr>
2026-01-21 15:28:30 +0900

    black

--- lmms_eval/loggers/wandb_logger.py
+++ lmms_eval/loggers/wandb_logger.py
@@ -45,11 +45,7 @@ class WandbLogger:
             if Version(wandb.__version__) < Version("0.13.6"):
                 wandb.require("report-editing:v0")
         except Exception as e:
-            logger.warning(
-                "To use the wandb reporting functionality please install wandb>=0.13.6.\n"
-                "To install the latest version of wandb run `pip install wandb --upgrade`\n"
-                f"{e}"
-            )
+            logger.warning("To use the wandb reporting functionality please install wandb>=0.13.6.\n" "To install the latest version of wandb run `pip install wandb --upgrade`\n" f"{e}")
 
         self.wandb_args: Dict[str, Any] = kwargs
 
@@ -167,9 +163,7 @@ class WandbLogger:
         """Log results as JSON artifact to W&B."""
         import wandb
 
-        dumped = json.dumps(
-            self.results, indent=2, default=_handle_non_serializable, ensure_ascii=False
-        )
+        dumped = json.dumps(self.results, indent=2, default=_handle_non_serializable, ensure_ascii=False)
         artifact = wandb.Artifact("results", type="eval_results")
         with artifact.new_file("results.json", mode="w", encoding="utf-8") as f:
             f.write(dumped)
@@ -191,9 +185,7 @@ class WandbLogger:
         # Log the results dict as json to W&B Artifacts
         self._log_results_as_artifact()
 
-    def _generate_dataset(
-        self, data: List[Dict[str, Any]], config: Dict[str, Any]
-    ) -> pd.DataFrame:
+    def _generate_dataset(self, data: List[Dict[str, Any]], config: Dict[str, Any]) -> pd.DataFrame:
         """Generate a dataset from evaluation data.
 
         Args:
@@ -226,32 +218,13 @@ class WandbLogger:
         if config["output_type"] == "loglikelihood":
             instance = [x["arguments"][0][0] for x in data]
             labels = [x["arguments"][0][1] for x in data]
-            resps = [
-                f"log probability of continuation is {x['resps'][0][0][0]} "
-                + "\n\n"
-                + "continuation will {} generated with greedy sampling".format(
-                    "not be" if not x["resps"][0][0][1] else "be"
-                )
-                for x in data
-            ]
-            filtered_resps = [
-                f"log probability of continuation is {x['filtered_resps'][0][0]} "
-                + "\n\n"
-                + "continuation will {} generated with greedy sampling".format(
-                    "not be" if not x["filtered_resps"][0][1] else "be"
-                )
-                for x in data
-            ]
+            resps = [f"log probability of continuation is {x['resps'][0][0][0]} " + "\n\n" + "continuation will {} generated with greedy sampling".format("not be" if not x["resps"][0][0][1] else "be") for x in data]
+            filtered_resps = [f"log probability of continuation is {x['filtered_resps'][0][0]} " + "\n\n" + "continuation will {} generated with greedy sampling".format("not be" if not x["filtered_resps"][0][1] else "be") for x in data]
         elif config["output_type"] == "multiple_choice":
             instance = [x["arguments"][0][0] for x in data]
-            choices = [
-                "\n".join([f"{idx}. {y[1]}" for idx, y in enumerate(x["arguments"])])
-                for x in data
-            ]
+            choices = ["\n".join([f"{idx}. {y[1]}" for idx, y in enumerate(x["arguments"])]) for x in data]
             resps = [np.argmax([n[0][0] for n in x["resps"]]) for x in data]
-            filtered_resps = [
-                np.argmax([n[0] for n in x["filtered_resps"]]) for x in data
-            ]
+            filtered_resps = [np.argmax([n[0] for n in x["filtered_resps"]]) for x in data]
         elif config["output_type"] == "loglikelihood_rolling":
             instance = [x["arguments"][0][0] for x in data]
             resps = [x["resps"][0][0] for x in data]
@@ -282,9 +255,7 @@ class WandbLogger:
 
         return pd.DataFrame(df_data)
 
-    def _log_samples_as_artifact(
-        self, data: List[Dict[str, Any]], task_name: str
-    ) -> None:
+    def _log_samples_as_artifact(self, data: List[Dict[str, Any]], task_name: str) -> None:
         import wandb
 
         # log the samples as an artifact
@@ -295,9 +266,7 @@ class WandbLogger:
             ensure_ascii=False,
         )
         artifact = wandb.Artifact(f"{task_name}", type="samples_by_task")
-        with artifact.new_file(
-            f"{task_name}_eval_samples.json", mode="w", encoding="utf-8"
-        ) as f:
+        with artifact.new_file(f"{task_name}_eval_samples.json", mode="w", encoding="utf-8") as f:
             f.write(dumped)
         self.run.log_artifact(artifact)
         # artifact.wait()
@@ -308,9 +277,7 @@ class WandbLogger:
         Args:
             samples (Dict[str, List[Dict[str, Any]]]): Evaluation samples for each task.
         """
-        task_names: List[str] = [
-            x for x in self.task_names if x not in self.group_names
-        ]
+        task_names: List[str] = [x for x in self.task_names if x not in self.group_names]
 
         ungrouped_tasks = []
         tasks_by_groups = {}
@@ -343,9 +310,7 @@ class WandbLogger:
             grouped_df = pd.DataFrame()
             for task_name in grouped_tasks:
                 eval_preds = samples[task_name]
-                df = self._generate_dataset(
-                    eval_preds, self.task_configs.get(task_name)
-                )
+                df = self._generate_dataset(eval_preds, self.task_configs.get(task_name))
                 df["group"] = group
                 df["task"] = task_name
                 grouped_df = pd.concat([grouped_df, df], ignore_index=True)
